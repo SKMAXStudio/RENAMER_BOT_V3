053c700e3da6c92bf7e69616d9365892b19f3a18
SKMAX Studio Official™ <118368174+SKMAXStudio@users.noreply.github.com>
2023-04-29 11:52:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,13 +4,13 @@ import os
 
 from plugins.cb_data import app as Client2
 
-TOKEN = os.environ.get("TOKEN", "")
+TOKEN = os.environ.get("TOKEN", "6291649683:AAG-T-wddjXn2PC_FIVujMEXyKk2pDqTJuc")
 
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "29034718"))
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "5719d4400fd38bf1eb353d05174562ed")
 
-STRING = os.environ.get("STRING", "")
+STRING = os.environ.get("STRING", "@SKMAX_Studio")
 
 
 bot = Client(
